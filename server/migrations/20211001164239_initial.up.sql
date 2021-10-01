a081001abe958e7ea5a786a4c7f1beff9d4b8983
AlyssaThi <aruefenacht14@gmail.com>
2021-10-01 16:50:02 +0800

    db

---
+++ server/migrations/20211001164239_initial.up.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE IF NOT EXISTS users(
+    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    name TEXT NOT NULL,
+    username TEXT NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS articles(
+    id UUID NOT NULL DEFAULT uuid_generate_v4(),
+    title TEXT NOT NULL,
+    date_created TIMESTAMP NOT NULL,
+    date_edited TIMESTAMP,
+    body TEXT NOT NULL,
+    category TEXT NOT NULL
+);
