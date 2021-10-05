ae6e577b1a678b6d905de09e646fc31527472a38
Alyssa Ninja Linux <aruefenacht14@gmail.com>
2021-10-05 12:17:20 +0800

    .

--- web/src/components/articleCard/index.tsx
+++ web/src/components/articleCard/index.tsx
@@ -1,9 +1,17 @@
+import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
 
-export function ArticleCard() {
+export function ArticleCard(): JSX.Element {
+	async function handleCardClick() {
+		console.log("click");
+	}
 	return (
-		<div>
-			<h1>Article Card</h1>
-		</div>
+		<Card onClick={handleCardClick}>
+			<CardMedia />
+			<CardContent>
+				<Typography variant="h3">Title</Typography>
+				<Typography variant="subtitle1">Snippet</Typography>
+			</CardContent>
+		</Card>
 	);
 }
