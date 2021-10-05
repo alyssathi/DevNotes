7fcf5029c9894427801e6d2d701b11f0b0afcfb0
Alyssa Ninja Linux <aruefenacht14@gmail.com>
2021-10-05 13:09:46 +0800

    sx for components

--- web/src/components/articleCard/index.tsx
+++ web/src/components/articleCard/index.tsx
@@ -1,12 +1,13 @@
 import { Card, CardContent, CardMedia, Typography } from "@mui/material";
 import React from "react";
+import { sx } from "./articleCardCss";
 
 export function ArticleCard(): JSX.Element {
 	async function handleCardClick() {
 		console.log("click");
 	}
 	return (
-		<Card onClick={handleCardClick}>
+		<Card onClick={handleCardClick} sx={sx.container}>
 			<CardMedia />
 			<CardContent>
 				<Typography variant="h3">Title</Typography>
